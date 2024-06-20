6064fef326462de4d99c0a3c755444799cee1e91
leekbillow <645997108@qq.com>
2024-06-20 17:55:53 +0800

    格式化代码

--- fxxkLoginDialog.js
+++ fxxkLoginDialog.js
@@ -12,17 +12,15 @@
 // @updateURL    https://raw.githubusercontent.com/leekbillow/customTampermonkey/main/fxxkLoginDialog.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    switch(true)
-    {
-        case /\bblog\.csdn\.net\b(?!\.)/.test(location.hostname):
-        {
-            // CSDN
-            let csdnStyle=document.createElement('style');
-            csdnStyle.classList.add('Tampermonkey');
-            csdnStyle.innerHTML=`
+  switch (true) {
+    case /\bblog\.csdn\.net\b(?!\.)/.test(location.hostname): {
+      // CSDN
+      let csdnStyle = document.createElement("style");
+      csdnStyle.classList.add("Tampermonkey");
+      csdnStyle.innerHTML = `
               .passport-login-container,
               #passportbox,
               .toolbar-advert
@@ -38,16 +36,16 @@
                 display:none!important;
               }
             `;
-            document.head.append(csdnStyle);
-            break;
-        }
-        case /\bzhihu\.com\b(?!\.)/.test(location.hostname):
-        {
-            // 知乎
-            //添加限制样式
-            let bhuStyle=document.createElement('style');
-            bhuStyle.classList.add('Tampermonkey');
-            bhuStyle.innerHTML=`
+      document.head.append(csdnStyle);
+      break;
+    }
+    case /\bzhihu\.com\b(?!\.)/.test(location.hostname): {
+      // 知乎
+        debugger;
+        //添加限制样式
+      let bhuStyle = document.createElement("style");
+      bhuStyle.classList.add("Tampermonkey");
+      bhuStyle.innerHTML = `
               .Modal-wrapper,
               .Modal-backdrop,
               .signFlowModal
@@ -55,48 +53,44 @@
                   display:none!important;
               }
             `;
-            document.head.append(bhuStyle);
-            // 弹窗数量，专栏为2
-            let rubbishDialogQuantity=1+/\bzhuanlan\.zhihu\.com\b(?!\.)/.test(location.hostname),clicked=[];
-            // 取消首次自动弹出登录框,解除监听
-            let removeStyle=function(){bhuStyle.remove();},
-                targetNode = document.body,
-                observerOptions=
-                {
-                    childList: true,
-                    subtree: true
-                },
-                observer = new MutationObserver(function(mutationList,observer)
-                {
-                    let rubbishDialogCloseButtons=document.querySelectorAll('.Modal-closeButton');
-                    if(rubbishDialogCloseButtons.length>0)
-                    {
-                        [].forEach.call(rubbishDialogCloseButtons,E=>
-                        {
-                          if(clicked.includes(E)) return;
-                          E.click();
-                          clicked.push(E);
-                          if(--rubbishDialogQuantity<1)
-                          {
-                            observer.disconnect();
-                            let loginButton=document.querySelector('.AppHeader-login');
-                            loginButton && (loginButton.onclick=()=>removeStyle());
-                          }
-                        });
-                    }
-                    else return;
-                });
-            observer.observe(targetNode, observerOptions);
-            //3秒后移除监听
-            setTimeout(()=>(observer.disconnect(),removeStyle()),3000);
-            break;
-        }
-        case /\bjianshu\.com\b(?!\.)/.test(location.hostname):
-        {
-            // 简书
-            let jianshuStyle=document.createElement('style');
-            jianshuStyle.classList.add('Tampermonkey');
-            jianshuStyle.innerHTML=`
+      document.head.append(bhuStyle);
+      // 弹窗数量，专栏为2
+      let rubbishDialogQuantity = 1 + /\bzhuanlan\.zhihu\.com\b(?!\.)/.test(location.hostname),
+        clicked = [];
+      // 取消首次自动弹出登录框,解除监听
+      let removeStyle = function () {
+          bhuStyle.remove();
+        },
+        targetNode = document.body,
+        observerOptions = {
+          childList: true,
+          subtree: true,
+        },
+        observer = new MutationObserver(function (mutationList, observer) {
+          let rubbishDialogCloseButtons = document.querySelectorAll(".Modal-closeButton");
+          if (rubbishDialogCloseButtons.length > 0) {
+            [].forEach.call(rubbishDialogCloseButtons, (E) => {
+              if (clicked.includes(E)) return;
+              E.click();
+              clicked.push(E);
+              if (--rubbishDialogQuantity < 1) {
+                observer.disconnect();
+                let loginButton = document.querySelector(".AppHeader-login");
+                loginButton && (loginButton.onclick = () => removeStyle());
+              }
+            });
+          } else return;
+        });
+      observer.observe(targetNode, observerOptions);
+      //3秒后移除监听
+      setTimeout(() => (observer.disconnect(), removeStyle()), 3000);
+      break;
+    }
+    case /\bjianshu\.com\b(?!\.)/.test(location.hostname): {
+      // 简书
+      let jianshuStyle = document.createElement("style");
+      jianshuStyle.classList.add("Tampermonkey");
+      jianshuStyle.innerHTML = `
               ._1aCo37-mask,
               ._1aCo37-wrap
               {
@@ -107,23 +101,23 @@
                   overflow:auto!important
               }
             `;
-            document.head.append(jianshuStyle);
-            break;
-        }
-        case /\bjuejin\.cn\b(?!\.)/.test(location.hostname):
-        {
-            // 掘金
-            let juejinStyle=document.createElement('style');
-            juejinStyle.classList.add('Tampermonkey');
-            juejinStyle.innerHTML=`
+      document.head.append(jianshuStyle);
+      break;
+    }
+    case /\bjuejin\.cn\b(?!\.)/.test(location.hostname): {
+      // 掘金
+      let juejinStyle = document.createElement("style");
+      juejinStyle.classList.add("Tampermonkey");
+      juejinStyle.innerHTML = `
               .extension
               {
                   display:none!important
               }
             `;
-            document.head.append(juejinStyle);
-            break;
-        }
-        default:break;
+      document.head.append(juejinStyle);
+      break;
     }
+    default:
+      break;
+  }
 })();
